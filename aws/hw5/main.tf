3e829a50981b7f9e53a9393a8372093768f585c9
vesk18 <44535702+vesk18@users.noreply.github.com>
2025-04-23 12:56:09 -0700

    Update main.tf

--- aws/hw5/main.tf
+++ aws/hw5/main.tf
@@ -2,19 +2,42 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "username" {
-  type        = string
-  description = "Your login name for backend path"
-  default     = "vesk18"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 module "ec2_nginx" {
   source             = "./modules/ec2-nginx"
-  vpc_id             = "vpc-08027fb7ca8977cdd"
+  vpc_id             = aws_vpc.main.id
+  subnet_id          = aws_subnet.public.id
   list_of_open_ports = [80, 443]
 }
 
 output "instance_ip" {
   value = module.ec2_nginx.instance_ip
 }
-
